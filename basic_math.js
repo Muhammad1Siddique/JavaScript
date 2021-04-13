1bf205cce9abc099dca7bab6e3daaf6d3a7cc90d
Muhammad Siddique <48084054+Muhammad1Siddique@users.noreply.github.com>
2021-04-13 19:30:09 +0500

    basic math operation perform by me

---
+++ basic_math.js
@@ -0,0 +1,17 @@
+let a = 125;
+let b = 34;
+
+let sum = a + b;
+console.log(sum);
+
+let sub = a - b;
+console.log(sub);
+
+let mul = a * b;
+console.log(mul);
+
+let divid = a / b;
+console.log(divid);
+
+let reminder = a % b;
+console.log(reminder);
